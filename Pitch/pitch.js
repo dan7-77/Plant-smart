3e330b394c8a30cd0de96eeace94bd6a22df1626
dan7-77 <danieabreu151@gmail.com>
2025-06-22 21:21:50 -0300

    Add files via upload

--- Pitch/pitch.js
+++ Pitch/pitch.js
@@ -12,16 +12,19 @@
     }
 
 
-
-
-
-
-
-
-
     function showSection(sectionId) {
       document.querySelectorAll('.section').forEach(section => {
         section.style.display = 'none';
       });
       document.getElementById(sectionId).style.display = 'block';
     }
+
+
+
+
+
+
+
+
+
+    
